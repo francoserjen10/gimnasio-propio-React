49a0442f4008dffbd1ee3e0ac70993bc1d04a3c8
Franco_Serjen <francoserjen100@gmail.com>
2025-02-14 15:19:06 -0300

    Servicio de login terminado

---
+++ gym-front-end/src/app/services/auth-services/login-service.tsx
@@ -0,0 +1,18 @@
+import axiosInstance from "../conexion-db/axios";
+import { isAxiosError } from "axios";
+
+export const loginService = async (email: string, password: string) => {
+    try {
+        const response = await axiosInstance.post("/login/access", { email, password }, { withCredentials: true, });
+        console.log('servicio', response.data.messenge);
+        return response.data;
+    } catch (error) {
+        if (isAxiosError(error)) {
+            console.error("Error en loginService:", error.response?.data?.message || "Error desconocido en la autenticación");
+            throw new Error(error.response?.data?.message || "Error en la autenticación");
+        } else {
+            console.error("Error inesperado:", error);
+            throw new Error("Error inesperado en el servicio de login");
+        }
+    }
+};
