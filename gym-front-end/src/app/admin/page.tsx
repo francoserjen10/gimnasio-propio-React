ee07b1bd305a99d1f628f81393dbc88e6bcac59c
Franco_Serjen <francoserjen100@gmail.com>
2025-04-11 14:00:06 -0300

    Protección de rutas utilizando High Order Component

--- gym-front-end/src/app/admin/page.tsx
+++ gym-front-end/src/app/admin/page.tsx
@@ -1,5 +1,10 @@
-export default function HomeAdmin() {
+"use client";
+import { withAuth } from "@/HOC/withAuth";
+
+function HomeAdmin() {
     return (
         <h1>ADMINISTRADOR</h1>
     );
 }
+
+export default withAuth(HomeAdmin, [1]);
