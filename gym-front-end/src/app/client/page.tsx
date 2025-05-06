7be8c1900fe55b515ae1ea16b00b490dcdd2d243
Franco_Serjen <francoserjen100@gmail.com>
2025-05-06 13:04:51 -0300

    Implementación de logout en el home del cliente

--- gym-front-end/src/app/client/page.tsx
+++ gym-front-end/src/app/client/page.tsx
@@ -1,9 +1,26 @@
 "use client";
+import { useUser } from "@/context/user-context";
 import { withAuth } from "@/HOC/withAuth";
+import { logOut } from "@/services/auth/logout-service";
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
+import { useRouter } from "next/navigation";
 
 function HomeClient() {
+    const router: AppRouterInstance = useRouter();
+    const { setUser } = useUser();
+
     return (
-        <h1>Usuario</h1>
+        <>
+            <h1>Usuario</h1>
+
+            <button onClick={async () => {
+                setUser(null);
+                await logOut();
+                router.push("/login");
+            }}>
+                Cerrar sesion
+            </button>
+        </>
     );
 }
 
