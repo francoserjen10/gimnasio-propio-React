e087f3c89943ebb8238835d86ec50d12eaf0b659
Franco_Serjen <francoserjen100@gmail.com>
2025-06-17 11:58:34 -0300

    Implementación de título para el layout de administrador

--- gym-front-end/src/components/layouts/layoutAdmin.tsx
+++ gym-front-end/src/components/layouts/layoutAdmin.tsx
@@ -5,7 +5,14 @@ export default function LayoutAdmin({ children }: { children: React.ReactNode })
     return (
         <div className="flex">
             <SidebarAdmin />
-            <main className="flex-1 overflow-auto bg-gray-900">{children}</main>
+            <main className="flex-1 overflow-auto bg-gray-900 text-white">
+                <div className="sticky top-0 z-10 border-b border-gray-700 px-6 py-4 shadow-md">
+                    <h1 className="text-2xl font-bold text-center">Panel Administrativo</h1>
+                </div>
+                <div className="p-6">
+                    {children}
+                </div>
+            </main>
         </div>
     );
 }
